ca195d35ccb23004d06c0994e9e9c93e07056261
Natália L. Moreno <63927404+natalialopesmoreno@users.noreply.github.com>
2021-08-11 15:27:01 -0300

    Subindo versão inicial do projeto

---
+++ src/main/java/com/algaworks/algafood/JavaWebScrapingApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebScrapingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebScrapingApplication.class, args);
+	}
+
+}
